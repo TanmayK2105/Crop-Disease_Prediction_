ec5c7bf1f38646cf225f15fe0c9453939039e037
TanmayK2105 <111627329+TanmayK2105@users.noreply.github.com>
2023-11-28 12:46:59 +0530

    ok

--- app.py
+++ app.py
@@ -1,26 +1,57 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request
 import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from PIL import Image
 import io
+from tensorflow.keras.models import load_model
+from flask import send_from_directory
+
+
 
 app = Flask(__name__)
 
-# Load models
-model_tomato = load_model('model.h5')
+# Load models for different plants
+model_tomato = load_model('tomato.h5')
 model_potato = load_model('potatoes.h5')
+# model_apple = load_model('apple.h5')
+# model_grapes = load_model('grapes_disease_model_with_early_stopping.h5')
+# model_oranges = load_model('model_oranges.h5')
+# model_soybean = load_model('model_soybean.h5')
+# model_strawberry = load_model('model_strawberry.h5')
+# model_peach = load_model('model_peach.h5')
+# model_corn = load_model('corn.h5')
 
-# Class labels for tomato and potato models
-tomato_class_labels = ["Tomato - Bacteria Spot Disease", "Tomato - Early Blight Disease", "Tomato - Healthy and Fresh",
-                           "Tomato - Late Blight Disease", "Tomato - Leaf Mold Disease", "Tomato - Septoria Leaf Spot Disease",
-                           "Tomato - Target Spot Disease", "Tomato - Tomoato Yellow Leaf Curl Virus Disease",
-                           "Tomato - Tomato Mosaic Virus Disease", "Tomato - Two Spotted Spider Mite Disease"]
+# Class labels for different plants
+tomato_class_labels = ["Tomato : Bacterial Spot", "Tomato : Early Blight", "Tomato : Healthy", "Tomato : Late Blight", "Tomato : Leaf Mold", "Tomato : Septoria Leaf Spot", "Tomato : Target Spot", "Tomato : Yellow Leaf Curl Virus", "Tomato : Mosaic Virus", "Tomato : Spider Mites | Two-Spotted Spider Mite"]
 
-potato_class_labels = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+potato_class_labels = ['Potato : Early Blight', 'Potato : Late Blight', 'Potato : Healthy']
 
-def predict_plant_tomato(image_base64, model, class_labels):
-    img = image.load_img(io.BytesIO(image_base64), target_size=(128, 128))
-    img_array = image.img_to_array(img) / 255
+apple_class_labels = ['Apple : Scab',
+ 'Apple : Black Rot',
+ 'Apple : Cedar rust',
+ 'Apple : Healthy']
+
+grapes_class_labels = ['Grape : Black Rot',
+ 'Grape : Esca | Black Measles',
+ 'Grape : Leaf Blight | Isariopsis Leaf Spot',
+ 'Grape : Healthy']
+
+oranges_class_labels = ['Orange - HLB (Citrus Greening)', 'Orange - Healthy']
+
+soybean_class_labels = ['Soybean - Healthy', 'Soybean - Mosaic Virus']
+
+strawberry_class_labels = ['Strawberry - Healthy', 'Strawberry - Leaf Scorch']
+
+peach_class_labels = ['Peach - Bacterial Spot', 'Peach - Healthy']
+
+corn_class_labels = ['Corn : Cercospora Leaf Spot | Gray Leaf Spot',
+ 'Corn : Common Rust',
+ 'Corn : Northern Leaf Blight',
+ 'Corn : Healthy']
+
+def predict_tomato(image_bytes, model, class_labels):
+    img = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    img = img.resize((128, 128))  # Resize the image to match the model's expected sizing
+    img_array = np.array(img) / 255.0  # Normalize the image
     img_array = np.expand_dims(img_array, axis=0)
     result = model.predict(img_array)
     pred = np.argmax(result, axis=1)
@@ -28,46 +59,72 @@ def predict_plant_tomato(image_base64, model, class_labels):
     probability = float(result[0, int(pred)])  # Extract the probability for the predicted class
     return predicted_class, probability
 
-def predict_plant_potato(image_base64, model, class_labels):
-    img = image.load_img(io.BytesIO(image_base64), target_size=(256, 256))
-    img_array = image.img_to_array(img)
+def predict_plant(image_bytes, model, class_labels):
+    img = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    img = img.resize((256, 256))  # Resize the image to match the model's expected sizing
+    img_array = np.array(img) / 255.0  # Normalize the image
     img_array = np.expand_dims(img_array, axis=0)
     result = model.predict(img_array)
-    predicted_class = class_labels[np.argmax(result)]
-    probability = float(np.max(result))
+    pred = np.argmax(result, axis=1)
+    predicted_class = class_labels[int(pred)]
+    probability = float(result[0, int(pred)])  # Extract the probability for the predicted class
     return predicted_class, probability
-
-
-def interpret_prediction(prediction, class_labels):
-    max_index = np.argmax(prediction)
-    max_class = class_labels[max_index]
-    max_probability = float(prediction[max_index])
-    interpreted_result = {"class": max_class, "probability": max_probability}
-    return interpreted_result
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict_tomato', methods=['GET', 'POST'])
-def predict_tomato():
-    if request.method == 'POST':
-        image_base64 = request.files['image'].read()
-        prediction, probability = predict_plant_tomato(image_base64, model_tomato, tomato_class_labels)
-        interpreted_result = {"class": prediction, "probability": probability}
-        return jsonify(interpreted_result)
-    else:
-        return render_template('predict_tomato.html')
+@app.route('/get_csv/<filename>')
+def get_csv(filename):
+    try:
+        return send_from_directory('', filename)
+    except Exception as e:
+        return str(e), 500
 
-@app.route('/predict_potato', methods=['GET', 'POST'])
-def predict_potato():
-    if request.method == 'POST':
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        if 'image' not in request.files:
+            return render_template('error.html', message="Error: No image file provided.")
+
+        crop_type = request.form['crop_type']
         image_base64 = request.files['image'].read()
-        prediction, probability = predict_plant_potato(image_base64, model_potato, potato_class_labels)
-        interpreted_result = {"class": prediction, "probability": probability}
-        return jsonify(interpreted_result)
-    else:
-        return render_template('predict_potato.html')
+
+        if crop_type == 'tomato':
+            prediction, _ = predict_tomato(image_base64, model_tomato, tomato_class_labels)
+            return render_template('predict.html', crop_type='Tomato', prediction=prediction)
+        elif crop_type == 'potato':
+            prediction, _ = predict_plant(image_base64, model_potato, potato_class_labels)
+            return render_template('predict.html', crop_type='Potato', prediction=prediction)
+        # elif crop_type == 'apple':
+        #     prediction, _ = predict_plant(image_base64, model_apple, apple_class_labels)
+        #     return render_template('predict.html', crop_type='Apple', prediction=prediction)
+        # elif crop_type == 'grapes':
+        #     prediction, _ = predict_plant(image_base64, model_grapes, grapes_class_labels)
+        #     return render_template('predict.html', crop_type='Grapes', prediction=prediction)
+        # elif crop_type == 'oranges':
+        #     prediction, _ = predict_plant(image_base64, model_oranges, oranges_class_labels)
+        #     return render_template('predict.html', crop_type='Oranges', prediction=prediction)
+        # elif crop_type == 'soybean':
+        #     prediction, _ = predict_plant(image_base64, model_soybean, soybean_class_labels)
+        #     return render_template('predict.html', crop_type='Soybean', prediction=prediction)
+        # elif crop_type == 'strawberry':
+        #     prediction, _ = predict_plant(image_base64, model_strawberry, strawberry_class_labels)
+        #     return render_template('predict.html', crop_type='Strawberry', prediction=prediction)
+        # elif crop_type == 'peach':
+        #     prediction, _ = predict_plant(image_base64, model_peach, peach_class_labels)
+        #     return render_template('predict.html', crop_type='Peach', prediction=prediction)
+        # elif crop_type == 'corn':
+        #     prediction, _ = predict_plant(image_base64, model_corn, corn_class_labels)
+        #     return render_template('predict.html', crop_type='Corn', prediction=prediction)
+        else:
+            return render_template('error.html', message="Error: Invalid crop type.")
+
+    except Exception as e:
+        print(f"Error processing prediction: {str(e)}")
+        return render_template('error.html', message=f"Error: {str(e)}")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port=8080)
